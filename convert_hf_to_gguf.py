0ea4ea3b8005a9d6529d613c35045d05098757fe
Bertil Muth <bertil@requirementsascode.org>
2024-05-13 20:09:08 +0200

    WiP

--- convert_hf_to_gguf.py
+++ convert_hf_to_gguf.py
@@ -1,18 +1,18 @@
-import importlib.util
-spec = importlib.util.spec_from_file_location("module.name", "./llama.cpp/convert-llama-ggml-to-gguf.py")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-
 from llmtuner.chat import ChatModel
 from llmtuner.extras.misc import torch_gc
 from huggingface_hub import snapshot_download
+from scripts.llama_cpp import convert_hf_to_gguf
+import argparse
 
 base_model_name="microsoft/Phi-3-mini-4k-instruct"
 hf_model_id="bertilmuth/phi"
 
+# Download the model from Huggingface
 snapshot_download(repo_id=hf_model_id, local_dir="hf_model", revision="main")
 
-module.main("hf_model --outfile model.gguf")
+# Convert the model to GGUF format, 
+# using an adapted version of convert-hf-to-gguf.py (of llama.cpp)
+convert_hf_to_gguf.convert_model("hf_model", "model.gguf")
   
 
   
