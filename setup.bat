756c1d99c943da72151b3100a5e68183e8d7ccd6
Bertil Muth <bertil@requirementsascode.org>
2024-05-13 23:20:28 +0200

    WiP

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
-git clone https://github.com/ggerganov/llama.cpp scripts
+git clone https://github.com/ggerganov/llama.cpp scripts/llama.cpp
 pip install -r scripts/llama.cpp/requirements.txt
 pip install llmtuner
 
