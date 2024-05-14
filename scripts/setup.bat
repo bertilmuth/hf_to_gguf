c022ef72aaeb7059ffb3dccaa5e414582e8aab19
Bertil Muth <bertil@requirementsascode.org>
2024-05-14 09:24:24 +0200

    Include conda setup

---
+++ scripts/setup.bat
@@ -0,0 +1,18 @@
+:: Run the script (in the current directory) 
+:: to setup the conda environment if needed
+call "%~dp0setup_conda.bat" __convert_hf_to_gguf__
+
+:: Activate the environment
+call conda activate __convert_hf_to_gguf__
+
+:: Verify the activation
+if "%CONDA_DEFAULT_ENV%"=="__convert_hf_to_gguf__" (
+    echo Conda environment activated.
+) else (
+    echo Failed to activate conda environment.
+)
+
+git clone https://github.com/ggerganov/llama.cpp scripts/llama.cpp >nul 2>&1
+pip install -r scripts/llama.cpp/requirements.txt
+pip install llmtuner
+
---
+++ scripts/setup_conda.bat
@@ -0,0 +1,18 @@
+@echo off
+
+if "%~1"=="" (
+    echo Usage: %0 environment_name
+    exit /b 1
+)
+
+set "ENV_NAME=%~1"
+
+:: Check if the environment exists
+conda env list | findstr /c:"%ENV_NAME%" >nul
+if %ERRORLEVEL%==0 (
+    echo INFO: Environment %ENV_NAME% already exists.
+) else (
+    echo Creating new environment: %ENV_NAME%
+    :: Create the new environment with Python 3.10 (or any version you need)
+    conda create -n %ENV_NAME% python=3.10 -y
+)
