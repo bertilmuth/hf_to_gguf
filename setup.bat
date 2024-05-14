7d9d8766e0879f827fd81458fd3df9f6d6474080
Bertil Muth <bertil@requirementsascode.org>
2024-05-14 08:36:45 +0200

    Silently ignore the output of git clone

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
-git clone https://github.com/ggerganov/llama.cpp scripts/llama.cpp
+git clone https://github.com/ggerganov/llama.cpp scripts/llama.cpp >nul 2>&1
 pip install -r scripts/llama.cpp/requirements.txt
 pip install llmtuner
 
