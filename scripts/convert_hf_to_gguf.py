375cbc0d87f0da1095a9bbc7921eb73d0020548c
Bertil Muth <bertil@requirementsascode.org>
2024-05-15 12:17:21 +0200

    Fix path problems

--- scripts/convert_hf_to_gguf.py
+++ scripts/convert_hf_to_gguf.py
@@ -1,8 +1,12 @@
-import os, argparse, subprocess, shutil
+import os, argparse, subprocess
 from pathlib import Path
+from distutils.dir_util import copy_tree
 from huggingface_hub import snapshot_download
 
 def main():
+    hf_models = "hf_models"
+    gguf_models = "gguf_models"
+
     # Process the command line arguments
     parser = argparse.ArgumentParser(description='Process a model ID for Hugging Face.')
     parser.add_argument('hf_model_id', type=str, help='The Hugging Face model ID')
@@ -11,23 +15,23 @@ def main():
     hf_model_id = args.hf_model_id
     
     # Download the model from Hugging Face
-    local_hf_source_model_root_path = f"hf_models/{hf_model_id}"
-    snapshot_download(repo_id=hf_model_id, local_dir=local_hf_source_model_root_path, revision="main")
+    hf_model_directory = f"{hf_models}/{hf_model_id}"
+    snapshot_download(repo_id=hf_model_id, local_dir=hf_model_directory, revision="main")
 
     # Define variables & create GGUF model directory
     hf_model_name = extractModelName(hf_model_id)
-    gguf_model_directory_path = f"gguf_models/{hf_model_id}"
-    create_directory_if_not_exists(gguf_model_directory_path)
+    gguf_model_directory = f"{gguf_models}/{hf_model_id}"
+    create_directory_if_not_exists(gguf_model_directory)
 
     # Convert the model to GGUF format, using llama.cpp
-    gguf_model_path = f"{gguf_model_directory_path}/{hf_model_name}.gguf"
+    gguf_model_path = f"{gguf_model_directory}/{hf_model_name}.gguf"
     converter = ["python", "scripts/llama.cpp/convert-hf-to-gguf.py", 
-               "--outfile", gguf_model_path, "--outtype", "f16", local_hf_source_model_root_path]
+               "--outfile", gguf_model_path, "--outtype", "f16", hf_model_directory]
     run(converter)
     
     # Automatically install 
-    if directory_exists(gguf_model_directory_path):
-      install_to_lmstudio(gguf_model_directory_path)
+    if directory_exists(gguf_models):
+      install_to_lmstudio(gguf_models)
 
 def extractModelName(hf_model_id):
     parts = hf_model_id.split('/', 1)
@@ -48,17 +52,15 @@ def run(commandline_command):
     print(result.stdout)
     print(result.stderr)
     
-def install_to_lmstudio(source_model_root_path):
+def install_to_lmstudio(gguf_models):
     user_home_dir = Path.home()
     lmstudio_models_dir = user_home_dir / '.cache' / 'lm-studio' / 'models'
     
     if not directory_exists(lmstudio_models_dir):
       return
-
-    source_dir = Path(source_model_root_path)
     
-    shutil.copytree(source_dir, lmstudio_models_dir)
-    print(f"All files from {source_model_root_path} have been copied to {lmstudio_models_dir}")
+    copy_tree(gguf_models, str(lmstudio_models_dir.resolve()))
+    print(f"All files from {gguf_models} have been copied to {lmstudio_models_dir}")
 
 
 if __name__ == '__main__':
